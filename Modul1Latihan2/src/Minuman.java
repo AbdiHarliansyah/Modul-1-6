6897900a806cf9ac17d0b62c6fbbc05b255c7b9c
Muhammad Abdi Harliansyah <127309943+AbdiHarliansyah@users.noreply.github.com>
2023-03-08 20:13:50 +0700

    Add files via upload

---
+++ Modul1Latihan2/src/Minuman.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Minuman extends Hidangan{
+    public String disantap(){
+        return this.getNamaHidangan()+ "diminum";
+    }
+}
